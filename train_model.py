f6882d0dc37062f8c9b19d1712a643ad27e1379a
Tanishttha <tanishtthasehgal@gmail.com>
2026-08-25 10:00:00 +0530

    Update 1

--- train_model.py
+++ train_model.py
@@ -15,16 +15,3 @@ print("Cleaning dataset...")
 data = data[["title", "description", "fraudulent"]]
 data = data.dropna()
 
-data["text"] = data["title"] + " " + data["description"]
-
-print("Vectorizing text...")
-vectorizer = TfidfVectorizer(stop_words="english", max_features=5000)
-X = vectorizer.fit_transform(data["text"])
-y = data["fraudulent"]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-print("Training model...")
-model = LogisticRegression(class_weight="balanced", max_iter=1000)
-model.fit(X_train, y_train)
-
