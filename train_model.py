d4ebc5ba9031ab76b88a30b24de0bfd0320adef3
Tanishttha <tanishtthasehgal@gmail.com>
2026-08-26 14:00:00 +0530

    Update 3

--- train_model.py
+++ train_model.py
@@ -2,6 +2,3 @@ import pandas as pd
 df = pd.read_csv("fake_job_postings.csv")
 import numpy as np
 import os
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
