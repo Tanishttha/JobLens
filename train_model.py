07c774e602aa06397d91116592590e4eabcb4c4b
Tanishttha <tanishtthasehgal@gmail.com>
2026-08-26 12:00:00 +0530

    Update 2

--- train_model.py
+++ train_model.py
@@ -5,6 +5,3 @@ import os
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, accuracy_score
-import joblib
-
